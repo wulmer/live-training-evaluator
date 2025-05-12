0b16d2151cdc11eb317f346bc7b18706307e9f53
Wolfgang Ulmer <wolfgang.ulmer@de.bosch.com>
2025-05-12 16:54:58 +0000

    feat: Move label selection to layout

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,12 +1,50 @@
 <script lang="ts">
 	import type { LayoutProps } from './$types';
 	import { backendUrlStore, timeSpanMin } from '$lib/config.svelte';
+	import { onMount } from 'svelte';
 
 	let { children, data }: LayoutProps = $props();
 
 	if (data.backendUrl && data.backendUrl !== '') {
 		backendUrlStore.set(`http://${data.backendUrl}:8000`);
 	}
+
+	let currentLabels = $state([]) as string[];
+	let successfulConnection = $state(false);
+	let connectionStatus = $state(0);
+
+	async function updateLabels() {
+		let res: Response;
+		try {
+			const fetchURL = new URL(`${$backendUrlStore}/results/`);
+			fetchURL.searchParams.set('maxAgeMin', $timeSpanMin.toString());
+			console.log('Fetching data from backend...', fetchURL.toString());
+			res = await fetch(fetchURL);
+			successfulConnection = res.ok;
+			connectionStatus = res.status;
+		} catch (error) {
+			console.error('Error fetching data:', error);
+			successfulConnection = false;
+			connectionStatus = 0;
+			return;
+		}
+		const json = await res.json();
+
+		const labels: string[] = json.map((d) => d.label).sort((a, b) => a.localeCompare(b));
+		const uniqueLabels = [...new Set(labels)];
+		currentLabels = uniqueLabels;
+	}
+
+	onMount(() => {
+		updateLabels();
+		const timer = setInterval(() => {
+			updateLabels();
+		}, 2000);
+
+		return () => {
+			clearInterval(timer);
+		};
+	});
 </script>
 
 <section class="hero is-small">
@@ -30,6 +68,29 @@
 </section>
 
 {@render children()}
+
+<section class="section">
+	<h1 class="title">Available evaluations</h1>
+	<p class="subtitle">Click on a label to see the evaluation.</p>
+	{#if !successfulConnection}
+		<div class="notification is-warning">
+			<p>Could not connect to the backend. Please check your connection.</p>
+			{#if connectionStatus !== 0}
+				<p>Status code: {connectionStatus}</p>
+			{/if}
+		</div>
+	{:else if currentLabels.length === 0}
+		<p>No evaluations available.</p>
+	{:else}
+		<div class="field is-grouped">
+			{#each currentLabels as label}
+				<p class="control">
+					<a class="button" href="/{label}">{label}</a>
+				</p>
+			{/each}
+		</div>
+	{/if}
+</section>
 
 <section class="section">
 	<h1 class="title">Settings</h1>
